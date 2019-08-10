6d33b47aa05c5b75aab366cbf6ba2d01275ec03b
TK-yunyou <2578963817@qq.com>
2019-08-10 20:06:08 +0800

    计算1到100的质数之和,原理尚未弄懂

---
+++ CalPrime.py
@@ -0,0 +1,10 @@
+def is_prime(n):
+    for i in range(2,n):
+        if n % i == 0:
+            return False
+    return True
+sum=0
+for i in range(2,100):
+    if is_prime(i):
+        sum+=i
+print(sum)
